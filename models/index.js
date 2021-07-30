061d9735bb2ef4b4d2a40be735728e792b1597fc
rangamboa <rangamboa@gmail.com>
2021-07-29 21:19:55 -0700

    coding model associations

--- models/index.js
+++ models/index.js
@@ -5,12 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Categories have many Products
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  foreignKey: 'productTag_id',
+  onDelete: 'CASCADE',
+});
+
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  foreignKey: 'ProductTag_id',
+  onDelete: 'CASCADE',
+});
 
 module.exports = {
   Product,
