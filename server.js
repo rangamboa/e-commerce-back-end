e773ad16a8a0b323111424a2d341be7cfed4fdf3
rangamboa <rangamboa@gmail.com>
2021-07-29 21:03:38 -0700

    coding models

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
+const db = require('./models');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -15,3 +16,4 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('----- Now listening -----'));
 });
+
