8bce884b29f52f73a7602ca0008ef15504174b17
rangamboa <rangamboa@gmail.com>
2021-07-29 22:29:59 -0700

    more model associations

--- seeds/index.js
+++ seeds/index.js
@@ -3,11 +3,13 @@ const seedProducts = require('./product-seeds');
 const seedTags = require('./tag-seeds');
 const seedProductTags = require('./product-tag-seeds');
 
-const sequelize = require('../config/connection');
+const sequelize = require('../config/connection.js');
 
 const seedAll = async () => {
+
   await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
+
   await seedCategories();
   console.log('\n----- CATEGORIES SEEDED -----\n');
 
@@ -21,6 +23,7 @@ const seedAll = async () => {
   console.log('\n----- PRODUCT TAGS SEEDED -----\n');
 
   process.exit(0);
+
 };
 
 seedAll();
